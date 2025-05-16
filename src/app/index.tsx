59171f9a86a9f4148b3b527966164a3e817b3eac
vandodev <vandoaparecido@hotmail.com>
2025-05-16 07:41:05 -0300

    :moneybag: :credit_card: :moneybag: Passando parâmetro para a rota

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,8 +4,15 @@ import { router } from 'expo-router'
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
+      
       <Text>Expo Router</Text>
       <Button title="Nova meta" onPress={() => router.navigate('/target')} />
+
+      <Button
+        title="Transação"
+        onPress={() => router.navigate('/transaction/765890')}
+      />
+
     </View>
   )
 }
