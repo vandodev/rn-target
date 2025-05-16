293630cff2ed50fd5c17d89ed816367cbe608b8f
vandodev <vandoaparecido@hotmail.com>
2025-05-16 07:16:30 -0300

    :moneybag: :credit_card: :moneybag: Navegando com Expo Router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
+import { router } from 'expo-router'
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
       <Text>Expo Router</Text>
+      <Button title="Nova meta" onPress={() => router.navigate('/target')} />
     </View>
   )
 }
