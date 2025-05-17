1a7c40ccefab1df603db80f6ca1755c51ff1f0ba
vandodev <vandoaparecido@hotmail.com>
2025-05-17 08:35:10 -0300

    :moneybag: :credit_card: :moneybag: LinearGradient

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,11 @@
-import { Button, Text, View } from 'react-native'
-import { router } from 'expo-router'
+import { View } from 'react-native'
+import { HomeHeader } from '@/components/HomeHeader'
 
-import { fontFamily } from '@/theme/fontFamily'
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      
-      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>Expo Router</Text>
-      <Button title="Nova meta" onPress={() => router.navigate('/target')} />
-
-      <Button
-        title="Transação"
-        onPress={() => router.navigate('/transaction/765890')}
-      />
-
-      <Button
-        title="Progresso"
-        onPress={() => router.navigate('/in-progress/12')}
-      />
-
+    <View style={{ flex: 1, }}>
+      <HomeHeader />
     </View>
   )
 }
