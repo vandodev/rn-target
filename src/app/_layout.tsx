8d68772d6e5c867fa43652cb349fb258658dda6a
vandodev <vandoaparecido@hotmail.com>
2025-05-17 07:50:09 -0300

    :moneybag: :credit_card: :moneybag: Instalando fontes personalizadas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,23 @@
 import { colors } from '@/theme/colors'
 import { Stack } from 'expo-router'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <Stack
       screenOptions={{
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
 import { Button, Text, View } from 'react-native'
 import { router } from 'expo-router'
 
+import { fontFamily } from '@/theme/fontFamily'
+
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
       
-      <Text>Expo Router</Text>
+      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>Expo Router</Text>
       <Button title="Nova meta" onPress={() => router.navigate('/target')} />
 
       <Button
