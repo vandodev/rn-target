cd267cbdeaf8437e122c2881be09fd5b7d5dceb0
vandodev <vandoaparecido@hotmail.com>
2025-05-17 07:30:35 -0300

    :moneybag: :credit_card: :moneybag: Personalizando layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from 'expo-router';
+import { colors } from '@/theme/colors'
+import { Stack } from 'expo-router'
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.white },
+      }}
+    />
+  )
 }
