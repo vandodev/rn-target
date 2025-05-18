cadf6fd5e84785f672a7955426fb792e383717f9
vandodev <vandoaparecido@hotmail.com>
2025-05-18 07:16:10 -0300

    :moneybag: :credit_card: :moneybag: Componente Separator

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -2,6 +2,7 @@ import { Text, View } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { styles } from './styles'
 import { colors } from '@/theme/colors'
+import { Separator } from '../Separator'
 
 export type HomeHeaderProps = {
   total: string
@@ -21,6 +22,7 @@ export function HomeHeader({ data }: Props) {
             <Text style={styles.label}>Total que você possui</Text>
             <Text style={styles.total}>{data.total}</Text>
         </View>
+        <Separator color={colors.blue[400]} />
     </LinearGradient>
   )
 }
