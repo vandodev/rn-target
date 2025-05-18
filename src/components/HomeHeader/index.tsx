d2f953f8ff9234eed1fb4a8d60c7c03a1f35fe3b
vandodev <vandoaparecido@hotmail.com>
2025-05-18 07:02:08 -0300

    :moneybag: :credit_card: :moneybag: Exibinto tatal com type e props

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,7 +3,15 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { styles } from './styles'
 import { colors } from '@/theme/colors'
 
-export function HomeHeader() {
+export type HomeHeaderProps = {
+  total: string
+}
+
+type Props = {
+  data: HomeHeaderProps
+}
+
+export function HomeHeader({ data }: Props) {
   return (
     <LinearGradient
       colors={[colors.blue[500], colors.blue[800]]}
@@ -11,6 +19,7 @@ export function HomeHeader() {
     >
         <View>
             <Text style={styles.label}>Total que você possui</Text>
+            <Text style={styles.total}>{data.total}</Text>
         </View>
     </LinearGradient>
   )
