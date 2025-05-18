83895ba37f86a786e2f94124b5e1586ecf996c14
vandodev <vandoaparecido@hotmail.com>
2025-05-18 09:01:34 -0300

    :moneybag: :credit_card: :moneybag: Componente Summary

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,6 +3,7 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { styles } from './styles'
 import { colors } from '@/theme/colors'
 import { Separator } from '../Separator'
+import { Summary } from '../Summary'
 
 export type HomeHeaderProps = {
   total: string
@@ -23,6 +24,20 @@ export function HomeHeader({ data }: Props) {
             <Text style={styles.total}>{data.total}</Text>
         </View>
         <Separator color={colors.blue[400]} />
+
+        <View style={styles.summary}>
+            <Summary
+                data={{ label: 'Entradas', value: 'R$ 6.184,90' }}
+                icon={{ name: 'arrow-upward', color: colors.green[500] }}
+            />
+
+            <Summary
+            isLeft
+                data={{ label: 'Saídas', value: '-R$ 883,65' }}
+                icon={{ name: 'arrow-downward', color: colors.red[400] }}
+            />
+      </View>
+
     </LinearGradient>
   )
 }
