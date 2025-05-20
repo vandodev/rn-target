f99f4bb043e54895f24bbba50c92bb366f33c101
vandodev <vandoaparecido@hotmail.com>
2025-05-20 07:50:57 -0300

    :moneybag: :credit_card: :moneybag: Componente Transaction

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,13 +1,32 @@
 import { Button, Text, View } from 'react-native'
-import { useLocalSearchParams } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 import { PageHeader } from '@/components/PageHeader'
 import { Progress } from '@/components/Progress'
+import { List } from '@/components/List'
+import { Transaction, TransactionProps } from '@/components/Transaction'
+import { TransactionTypes } from '@/utils/TransactionTypes'
 
 const details = {
   current: 'R$ 580,00',
   target: 'R$ 1.780,00',
   percentage: 25,
 }
+
+const transactions: TransactionProps[] = [
+  {
+    id: '1',
+    value: 'R$ 20,00',
+    date: '12/04/25',
+    type: TransactionTypes.Output,
+  },
+  {
+    id: '2',
+    value: 'R$ 300,00',
+    date: '12/04/25',
+    description: 'CDB de 110% no banco XPTO',
+    type: TransactionTypes.Input,
+  },
+]
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>()
@@ -23,6 +42,20 @@ export default function InProgress() {
       />
 
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+         emptyMessage="Nenhuma transação. Toque em nova transação para guardar seu primeiro dinheiro aqui."
+      />
+
+      <Button
+        title="Nova transação"
+        onPress={() => router.navigate(`/transaction/${params.id}`)}
+      />
     </View>
   )
 }
