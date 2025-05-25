c0cec7abdbd0d6147ad4c234c75e0991ca69786f
vandodev <vandoaparecido@hotmail.com>
2025-05-25 07:37:59 -0300

    :moneybag: :credit_card: :moneybag: Método de cadastrar transação no banco

---
+++ src/database/useTransactionsDatabase.ts
@@ -0,0 +1,28 @@
+import { useSQLiteContext } from 'expo-sqlite'
+
+export type TransactionCreate = {
+  target_id: number
+  amount: number
+  observation?: string
+}
+
+export function useTransactionsDatabase() {
+  const database = useSQLiteContext()
+
+  async function create(data: TransactionCreate) {
+    const statement = await database.prepareAsync(`
+        INSERT INTO transactions
+          (target_id, amount, observation)
+        VALUES
+          ($target_id, $amount, $observation)
+      `)
+
+    statement.executeAsync({
+      $target_id: data.target_id,
+      $amount: data.amount,
+      $observation: data.observation,
+    })
+  }
+
+  return { create }
+}
